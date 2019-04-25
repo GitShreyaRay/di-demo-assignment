1b4e9bf8eec2062cd33db514836b85584a884817
GitShreyaRay <marsvsjupiter@gmail.com>
2019-04-25 19:48:43 +0530

    Corrected files again for other profiles

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,8 @@ import org.springframework.stereotype.Service;
 @Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
 
-    private GreetingRepository greetingRepository;
+    @Autowired
+	private GreetingRepository greetingRepository;
 
     @Override
     public String sayGreeting() {
